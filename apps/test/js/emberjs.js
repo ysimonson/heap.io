1e41e920e9a6ef119863760f6925ed9eb49b1e39
ysimonson <yusuf@dotcloud.com>
2012-05-02 14:56:53 -0700

    Added rpc server

--- apps/test/js/emberjs.js
+++ apps/test/js/emberjs.js
@@ -1,3 +1,18 @@
 function testEmber() {
-    
+    module("Ember.js adapter");
+
+    /*var emberReady = function() {
+        
+    };
+
+    var App = Em.Application.create({
+        ready: emberReady,
+        store: DS.HeapIOAdapter.create()
+    });
+
+    var Person = DS.Model.extend({
+        first: DS.attr("string"),
+        last: DS.attr("string"),
+        age: DS.attr("number")
+    });*/
 }
--- apps/test/js/patterns.js
+++ apps/test/js/patterns.js
@@ -26,28 +26,42 @@ function testPatterns() {
     });
 
     asyncTest("RPC", function() {
-        client.consume("patterns/rpc/silent", 0, function(error, key, value) {
-            ok(!error);
-        });  
+        heap.patterns.rpcServer(client, "patterns/rpc", {
+            silent: function(request) {
+                ok(!request.error);
+            },
 
-        client.consume("patterns/rpc/hello", 0, function(error, key, value) {
-            ok(!error);
-            equal(value.args, "RPC");
-            client.produce("patterns/rpc/foo2/" + value.username + "/" + value.requestId, "RPC");
-        });  
+            hello: function(request, name, age) {
+                ok(!request.error);
+                equal(name, "Joe");
+                equal(age, 24);
+                request.finish("Hello, " + name + " aged " + age);
+            },
 
-        var rpc = heap.patterns.rpc(client, "patterns/rpc");
+            badArgs: function(request) {
+                ok(!request.error);
+                equal(arguments.length, 1);
+            }
+        });
+
+        var rpc = heap.patterns.rpcClient(client, "patterns/rpc");
         var noError = function(error) { ok(!error); }
 
         rpc("silent", { request: noError });
 
         rpc("hello", {
             request: noError,
-            args: "RPC",
+            args: ["Joe", 24],
+
             response: function(error, value) {
                 ok(!error);
                 equal(value, "RPC");
             }
+        });
+
+        rpc("badArgs", {
+            request: noError,
+            args: {"bad": "arg"}
         });
 
         rpc("ignored", {
